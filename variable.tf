8859c69341d68c2fae6af24a8532d98a438d5fce
kokouapedo <apedokokouagbe@gmail.com>
2023-04-06 04:09:30 -0500

    terraform code

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "ami-type" {
+  description = "ami info to get from aws"
+  default = "ami-04581fbf744a7d11f"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "env" {
+  default = "dev"
+}
+variable "instance-name" {
+  default = "postgresql-server-dev"
+}
+variable "team" {
+  default = "dev-team"
+}
